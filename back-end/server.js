c45fc694a3f6b0e680afecad8c114f864afc7e6e
kinyarasam <samkinyara2000@gmail.com>
2022-06-04 20:16:04 +0300

    Error with Cors Solved

--- back-end/server.js
+++ back-end/server.js
@@ -6,8 +6,13 @@ const errorHandler = require('./middleware/errorhandler')
 
 require ('dotenv').config()
 
+app.use((req, res, next ) => {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	next()
+})
 app.use(express.json())
-app.use('/api/products', products)
+app.use('/products', products)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
@@ -15,6 +20,7 @@ const PORT = process.env.PORT || 5000
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI)
+		console.log('Connected to the DB successfully')
 		app.listen(PORT, () => {
 			console.log(`Server is running on ${PORT} url: http://localhost:${PORT}`)
 		})
